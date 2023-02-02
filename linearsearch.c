93d3399023d2255605f687d631296de064c429e0
ChavakulaNagasrimayi <113960939+ChavakulaNagasrimayi@users.noreply.github.com>
2023-02-02 12:45:03 +0530

    Add files via upload

---
+++ linearsearch.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int arr[10],n,key,i,a;
+	printf("Enter the elements in list:");
+	scanf("%d",&n);
+	printf("Elements in list:");
+	for(i=0;i<n;i++)
+	scanf("%d",&arr[i]);
+	printf("Enter the element to search:");
+	scanf("%d",&key);
+	if (arr[a]==key)
+	if(a<n)
+	{
+		printf("key element found %d",a);
+	}
+	else
+	printf("Key element not found");
+	getch();
+	return 0;
+	}
+
